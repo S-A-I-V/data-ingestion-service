36d69988599345c2a680b4e5daea23dc77a9ee5a
S-A-I-V <saideep.verma01@gmail.com>
2026-08-10 16:13:28 +0530

    test commit

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -110,7 +110,7 @@ export default function Home({ isAuthenticated }: Props) {
               viewport={{ once: true }}
               className="text-center text-[44px] md:text-[54px] lg:text-[64px] leading-[1.05] font-medium tracking-tight text-[#20201D]"
             >
-              <Highlight>Open Source</Highlight> Data
+              <Highlight>Open NFC</Highlight> Data
               <br />
               <Highlight>Engineering</Highlight> <Highlight>Platform</Highlight>
             </motion.h1>
